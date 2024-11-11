3ca4e1bee262edc6254175ce5796daa22b571c7f
dino-park <easternshy@gmail.com>
2024-11-11 17:01:07 +0900

    #1 - Data rest 테스트
    꼭 필요한 테스트는 아니나, 스터디 위해 작성

---
+++ src/test/java/com/greyson/projectboard/controller/DataRestTest.java
@@ -0,0 +1,47 @@
+package com.greyson.projectboard.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("Data Rest API테스트")
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest
+public class DataRestTest {
+
+    private final MockMvc mockMvc;
+
+    public DataRestTest(@Autowired MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+    @DisplayName("[api] 게시글 리스트 조회")
+    @Test
+    void given_when_then() throws Exception {
+        // Given
+
+        // When & Then
+        mockMvc.perform(get("/api/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+    @DisplayName("[api] 게시글 단일건 조회")
+    @Test
+    void given_whenOnce_then() throws Exception {
+        // Given
+
+        // When & Then
+        mockMvc.perform(get("/api/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+    }
+}
