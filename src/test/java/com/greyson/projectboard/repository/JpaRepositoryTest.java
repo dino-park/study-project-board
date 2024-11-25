0ca498fc45e9e3f5cef7fc268080a3d12bd94d30
dino-park <easternshy@gmail.com>
2024-11-25 19:00:34 +0900

    #18 - 회원 계정 엔티티와 게시글 미리 수정 및 푸시

--- src/test/java/com/greyson/projectboard/repository/JpaRepositoryTest.java
+++ src/test/java/com/greyson/projectboard/repository/JpaRepositoryTest.java
@@ -2,6 +2,7 @@ package com.greyson.projectboard.repository;
 
 import com.greyson.projectboard.config.JpaConfig;
 import com.greyson.projectboard.domain.Article;
+import com.greyson.projectboard.domain.UserAccount;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,11 +23,14 @@ class JpaRepositoryTest {
 
     private final ArticleRepository articleRepository;
     private final ArticleCommentRepository articleCommentRepository;
+    private final UserAccountRepository userAccountRepository;
 
     public JpaRepositoryTest(@Autowired ArticleRepository articleRepository,
-                             @Autowired ArticleCommentRepository articleCommentRepository) {
+                             @Autowired ArticleCommentRepository articleCommentRepository,
+                             @Autowired UserAccountRepository userAccountRepository) {
         this.articleRepository = articleRepository;
         this.articleCommentRepository = articleCommentRepository;
+        this.userAccountRepository = userAccountRepository;
     }
 
     @DisplayName("select Test")
@@ -48,8 +52,10 @@ class JpaRepositoryTest {
     void givenTestData_whenInserting_thenWorksFine(){
         // Given
         long previousCount = articleRepository.count();
+        UserAccount userAccount = userAccountRepository.save(UserAccount.of("dino", "pw", null, null, null));
+        Article article = Article.of(userAccount, "new article", "new content", "#spring");
         // When
-        Article savedArticle = articleRepository.saveAndFlush(Article.of("new article", "new content", "#spring"));
+        articleRepository.save(article);
 
         // Then
         assertThat(articleRepository.count()).isEqualTo(previousCount + 1);
