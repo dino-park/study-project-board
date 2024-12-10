1eaca59fcc6de531573654bc352744c9054c7d81
dino-park <easternshy@gmail.com>
2024-12-10 17:38:55 +0900

    #24 - 검색기능을 위해 SearchType옵션 전달
    - 게시판 페이지의 검색바에서 검색 종류를 드롭 다운 메뉴로 제공시, 해당 값을 뷰가 알게 하지 않고 서버로부터 받아서 쓰게 만들기 위해 값을 전달하는 로직을 테스트와 함께 추가

--- src/test/java/com/greyson/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/greyson/projectboard/controller/ArticleControllerTest.java
@@ -1,6 +1,7 @@
 package com.greyson.projectboard.controller;
 
 import com.greyson.projectboard.config.SecurityConfig;
+import com.greyson.projectboard.domain.type.SearchType;
 import com.greyson.projectboard.dto.ArticleWithCommentsDto;
 import com.greyson.projectboard.dto.UserAccountDto;
 import com.greyson.projectboard.service.ArticleService;
@@ -56,6 +57,28 @@ class ArticleControllerTest {
                 .andExpect(model().attributeExists("articles"))
                 .andExpect(model().attributeExists("paginationBarNumbers"));
         then(articleService).should().searchArticles(eq(null), eq(null), any(Pageable.class));
+        then(paginationService).should().getPaginationBarNumbers(anyInt(), anyInt());
+    }
+
+    @DisplayName("[view] [GET] 게시글 리스트 (게시판) 페이지 - 검색 기능")
+    @Test
+    public void givenSearchKeyword_whenSearchingArticleView_thenReturnsArticleView() throws Exception {
+        // Given
+        SearchType searchType = SearchType.TITLE;
+        String searchValue = "title";
+        given(articleService.searchArticles(eq(searchType), eq(searchValue), any(Pageable.class))).willReturn(Page.empty());
+        given(paginationService.getPaginationBarNumbers(anyInt(), anyInt())).willReturn(List.of(0,1,2,3,4));
+        // When & Then
+        mvc.perform(get("/articles")
+                        .queryParam("searchType", searchType.name())
+                        .queryParam("searchValue", searchValue)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("articles"))
+                .andExpect(model().attributeExists("searchTypes"));
+        then(articleService).should().searchArticles(eq(searchType), eq(searchValue), any(Pageable.class));
         then(paginationService).should().getPaginationBarNumbers(anyInt(), anyInt());
     }
 
