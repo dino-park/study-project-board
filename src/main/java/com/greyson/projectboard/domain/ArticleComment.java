6cf1443bc519b1955bce2e2baa2f2512ef7cb343
dino-park <easternshy@gmail.com>
2025-01-22 14:52:07 +0900

    #42 - 대댓글 뷰 구현

--- src/main/java/com/greyson/projectboard/domain/ArticleComment.java
+++ src/main/java/com/greyson/projectboard/domain/ArticleComment.java
@@ -5,7 +5,6 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
-import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
