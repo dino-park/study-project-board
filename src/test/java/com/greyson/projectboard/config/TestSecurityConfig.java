d83c7cc05ffddf98cbed19a2ba102a1dd0f591c9
dino-park <easternshy@gmail.com>
2025-02-03 18:03:08 +0900

    #50 - 인증 설정 업데이트: OAuth 인증 설정 추가
    기존 DB인증에서 OAuth 인증까지 받을수 있도록 설정 추가

--- src/test/java/com/greyson/projectboard/config/TestSecurityConfig.java
+++ src/test/java/com/greyson/projectboard/config/TestSecurityConfig.java
@@ -1,7 +1,8 @@
 package com.greyson.projectboard.config;
 
-import com.greyson.projectboard.domain.UserAccount;
+import com.greyson.projectboard.dto.UserAccountDto;
 import com.greyson.projectboard.repository.UserAccountRepository;
+import com.greyson.projectboard.service.UserAccountService;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.event.annotation.BeforeTestMethod;
@@ -15,16 +16,24 @@ import static org.mockito.BDDMockito.given;
 public class TestSecurityConfig {
 
     @MockBean private UserAccountRepository userAccountRepository;
+    @MockBean private UserAccountService userAccountService;
 
     @BeforeTestMethod
     public void securitySetUp() {
-        given(userAccountRepository.findById(anyString())).willReturn(Optional.of(UserAccount.of(
+        given(userAccountService.searchUser(anyString()))
+                .willReturn(Optional.of(createUserAccountDto()));
+        given(userAccountService.saveUser(anyString(), anyString(), anyString(), anyString(), anyString()))
+                .willReturn(createUserAccountDto());
+    }
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
                 "dinoTest",
                 "pw",
                 "dino-test@email.com",
                 "dino-test",
                 "test memo"
-        )));
+        );
     }
 
 }
